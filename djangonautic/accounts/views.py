3fac24635acd76f2165d79eb3f0a636b35116dc4
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-03-15 19:13:56 +0100

    actually log in

--- djangonautic/accounts/views.py
+++ djangonautic/accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
@@ -7,8 +8,8 @@ def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            # TODO(eik): log the user in
+            user = form.save()
+            login(request, user)
             return redirect('articles:list')
     else:
         form = UserCreationForm()
@@ -19,7 +20,8 @@ def login_view(request):
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            # TODO(eik): log in the user
+            user = form.get_user()
+            login(request, user)
             return redirect('articles:list')
     else:
         form = AuthenticationForm()
