084cf35349d342f6e6d88e2ce12243f05e526a88
Eik Hvattum Røgeberg <eik.rogeberg@gmail.com>
2021-03-15 19:39:16 +0100

    create article page

--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Article
 from django.http import HttpResponse
@@ -13,3 +14,8 @@ def article_detail(request, slug):
     # return HttpResponse(slug)
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/article_detail.html', {'article': article})
+
+
+@login_required(login_url='/accounts/login/')
+def article_create(request):
+    return render(request, 'articles/article_create.html')
